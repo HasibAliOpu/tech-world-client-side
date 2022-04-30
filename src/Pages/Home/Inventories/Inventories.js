f4be488fef18078be1b0087c22c5878078a2d1c4
H_A _Opu <wwwhasib4@gmail.com>
2022-05-01 00:30:16 +0600

    remove fake json and fetching heroku api

--- src/Pages/Home/Inventories/Inventories.js
+++ src/Pages/Home/Inventories/Inventories.js
@@ -4,15 +4,15 @@ import Inventory from "../Inventory/Inventory";
 const Inventories = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    fetch("products.json")
+    fetch("https://fierce-fjord-73876.herokuapp.com/item")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
   return (
     <div>
       <h1>Available Product {products.length}</h1>
-      <div className="grid grid-cols-3 p-10 ">
-        {products.map((Item) => (
+      <div className="grid grid-cols-3 gap-5 m-20">
+        {products.slice(6).map((Item) => (
           <Inventory key={Item.id} item={Item}></Inventory>
         ))}
       </div>
--- src/Pages/Home/Inventory/Inventory.js
+++ src/Pages/Home/Inventory/Inventory.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Inventory = ({ item }) => {
   const { name, img, description, price, seller, quantity } = item;
   return (
-    <div className="border border-slate-400">
+    <div className="border border-slate-400 p-8 rounded">
       <img src={img} alt="" />
       <h1>Name: {name}</h1>
       <h1>Price: ${price}</h1>
       <p>{description}</p>
       <p>quantity:{quantity}</p>
       <small>{seller}</small>
+      <Link to="/">Update</Link>
     </div>
   );
 };
